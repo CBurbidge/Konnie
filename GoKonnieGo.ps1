eeb9c8dc229486c05be6a84a846ea9d7aaad0ee8
Chester Burbidge <chester.burbidge@gmail.com>
2015-12-20 19:09:26 +0000

    added demonstration of how to call from powershell

---
+++ GoKonnieGo.ps1
@@ -0,0 +1,9 @@
+$execFile = ".\Konnie\bin\Debug\Konnie.exe"
+$params = "file1.konnie","file2.konnie","konnieTask"
+
+# Wait until the started process has finished
+& $execFile $params
+if (-not $?)
+{
+    # Show error message
+}
